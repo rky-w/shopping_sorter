7b5f1d23a6f18253c6b202049d643569c1068b43
Ricky <57967688+rky-w@users.noreply.github.com>
2021-05-03 22:10:39 +0100

    Product matching updates

--- sorter/sorter.py
+++ sorter/sorter.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import spacy
 import os
+import re
 
 os.system('python -m spacy download en_core_web_sm')
 
@@ -23,15 +24,31 @@ def clean_items(items):
     cleaned_items = []
     nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
     for item in items:
-        doc = nlp(item.lower())
+        strip_item = re.sub(r"[^a-z]", " ", item.lower())
+        doc = nlp(strip_item)
         lemma_item = " ".join([token.lemma_ for token in doc])
         cleaned_items.append(lemma_item)
     return cleaned_items
 
 
-if __name__ == '__main__':
-    locations = get_locations()
-    products = get_products()
+def item_matcher(items, locations):
+    items_df = pd.DataFrame(items, columns=['Product'])
+    match_products = items_df.set_index('Product').join(locations.set_index('CleanProduct')[['Aisle', 'Position']])
+    return match_products
 
-    print(clean_items(products))
-    print(clean_items(locations["Product"]))
+
+if __name__ == '__main__':
+
+    # Get location data
+    locations = get_locations()
+    locations["CleanProduct"] = clean_items(locations["Product"])
+
+    # Get shopping list products
+    products = get_products()
+    products = clean_items(products)
+
+    match_products = item_matcher(products, locations)
+
+    match_products.sort_values(by=['Aisle', 'Position'])
+
+print(match_products)
