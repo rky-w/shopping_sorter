0e0898058d323d19770567a35e48680341aa7eff
Ricky <57967688+rky-w@users.noreply.github.com>
2021-05-15 20:28:28 +0100

    Endpoint testing updates

--- sorter/endpoint.py
+++ sorter/endpoint.py
@@ -1,17 +1,12 @@
 
 from flask import Flask, Response, make_response
 from flask_restful import Resource, Api, reqparse
-from helpers import get_locations, get_products, clean_items, item_matcher, item_sorter
+from helpers import get_locations, clean_items, item_matcher, item_sorter
 import pandas as pd
-from io import StringIO
-import csv
-import json
 
 
 app = Flask(__name__)
 api = Api(app)
-
-
 
 
 class ListSort(Resource):
@@ -23,8 +18,7 @@ class ListSort(Resource):
         return Response(
             locations.to_csv(index=False),
             mimetype="text/csv",
-            headers={"Content-disposition":
-                         "attachment; filename=locations.csv"})
+            headers={"Content-disposition": "attachment; filename=locations.csv"})
 
 
     def post(self):
@@ -53,8 +47,46 @@ class ListSort(Resource):
 
         return resp
 
+    def put(self):
+        # Get item to add
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('store', required=True, type=str)  # add arguments
+        parser.add_argument('identifier', required=True, type=str)  # add arguments
+        parser.add_argument('product', required=True, type=str)  # add arguments
+        parser.add_argument('aisle', required=True, type=str)  # add arguments
+        parser.add_argument('position', required=True, type=int)  # add arguments
+        args = parser.parse_args()  # parse arguments to dictionary
 
-api.add_resource(ListSort, '/listsort')  # '/users' is our entry point for Users
+        # All location details
+        all_locations = pd.read_csv("./resources/product_locations.csv")
+
+        # New locations to add
+        new_location = pd.DataFrame([args])
+        new_location.columns = all_locations.columns
+
+        # Full outer join to add or update values
+        merge_locations = pd.merge(all_locations, new_location,
+                 how='outer',
+                 left_on=["Store", "Identifier", "Product"],
+                 right_on=["Store", "Identifier", "Product"],
+                 suffixes=['_old','_new'])
+
+        # Coalesce columns
+        merge_locations["Aisle"] = merge_locations["Aisle_new"].combine_first(merge_locations["Aisle_old"])
+        merge_locations["Position"] = merge_locations["Position_new"].combine_first(merge_locations["Position_old"])
+        merge_locations.drop(["Aisle_new", "Aisle_old", "Position_new", "Position_old"], axis=1, inplace=True)
+
+        # Reorder
+        merge_locations.sort_values(["Store", "Identifier", "Aisle", "Position"], inplace=True)
+
+        # Write back to CSV
+        resp = merge_locations.to_csv("./resources/product_locations.csv", index=False)
+
+        return resp
+
+
+
+api.add_resource(ListSort, '/listsort')  # '/listsort' is our entry point for Users
 
 
 if __name__ == '__main__':
